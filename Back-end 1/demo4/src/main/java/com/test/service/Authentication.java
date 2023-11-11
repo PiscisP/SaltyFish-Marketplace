80de33d9e57053cc5cf74e0211ef1f0dccbf0d80
Haoyang Zhou <hzhou3@scu.edu>
2023-11-10 18:50:50 -0800

    week 8

--- Back-end 1/demo4/src/main/java/com/test/service/Authentication.java
+++ Back-end 1/demo4/src/main/java/com/test/service/Authentication.java
@@ -9,24 +9,37 @@ import java.util.List;
 public class Authentication {
     private static class AuthenticatedUser
     {
+        public String username;
+        public long lastActionTimeMillis;
+
         public AuthenticatedUser(String username, long loginTime)
         {
             this.username = username;
             lastActionTimeMillis = loginTime;
         }
-        public String username;
-        public long lastActionTimeMillis;
     }
 
 
     // key is the ip address of the user
     private static HashMap<String, AuthenticatedUser> authenticatedUsers = new HashMap<String, AuthenticatedUser>();
 
-    private static boolean verifyPassword(String username, String password)
+    private static boolean verifyAccount(String username, String password)
     {
+        /*
+         * Function body
+         */
         return false;
     }
 
+    /*
+     * Behavior:
+     * 1. If the same account is logged in on some other device, then the new login attempt
+     *    will succeed and the account will be logged out on the other device
+     *
+     * 2. If the client is already logged in by one account and attempts to log in by another
+     *    account on the same device, then the new login attempt will succeed and the old
+     *    account will be logged out
+     */
     private static void addToAuthenticatedUsers(String ip, String username, long loginTime)
     {
         List<String> conflictedLogins = new ArrayList<String>();
@@ -48,15 +61,23 @@ public class Authentication {
 
 
 
-
+    /*
+     * @param ip       the ip address of the client
+     */
     public static void logout(String ip)
     {
         authenticatedUsers.remove(ip);
     }
 
+    /*
+     * @param ip       the ip address of the client
+     * @param username the username that the client used to log in
+     * @param password the password that the client used to log in
+     * @return         returns true if the login is success. returns false otherwise
+     */
     public static boolean login(String ip, String username, String password)
     {
-        boolean success = verifyPassword(username, password);
+        boolean success = verifyAccount(username, password);
         if (!success) { return false; }
 
 
@@ -66,9 +87,15 @@ public class Authentication {
         return true;
     }
 
-    // returns null if the client is not authenticated
+    /*
+     * @param ip       the ip address of the client
+     * @return         returns the username that is current logged in on the device. returns
+     *                 null if device is not logged in
+     */
     public static String getAuthenticatedUsername(String ip)
     {
+        if(!authenticatedUsers.containsKey(ip)) { return null; }
+
         return authenticatedUsers.get(ip).username;
     }
 
