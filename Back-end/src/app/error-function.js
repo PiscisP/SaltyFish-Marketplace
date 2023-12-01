264df9a248e5a4897cbaf95471f5771936761684
Ningjun <nelsonliningjun@gmail.com>
2023-12-01 00:03:44 -0800

    Initial commit on empty branch

--- Back-end/src/app/error-function.js
+++
@@ -1,7 +0,0 @@
-/* error submission function */
-const errorFunction = function (errorType, ctx) {
-  const error = new Error(errorType);
-  ctx.app.emit('error', error, ctx);
-};
-
-module.exports = errorFunction;
--- Back-end/src/main.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const { APP_PORT } = require('./app/config');
-require('./app/database');
-
-app.listen(APP_PORT, () => {
-  console.log(`koa serve initialized successfully，port number is：${APP_PORT}`);
-});
